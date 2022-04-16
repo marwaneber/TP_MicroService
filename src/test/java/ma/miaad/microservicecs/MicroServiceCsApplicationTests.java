6c0e59fe787b9cd1d5607f4d157769632357e3fc
Marwane Beriich <marber.mb11@gmail.com>
2022-04-16 00:51:09 +0000

    projet

---
+++ src/test/java/ma/miaad/microservicecs/MicroServiceCsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.miaad.microservicecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceCsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
